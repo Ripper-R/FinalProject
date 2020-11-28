2dcfeb4a1a93276ffbcc58d383d83ba5b76f7deb
Ripper-R <68725194+Ripper-R@users.noreply.github.com>
2020-11-28 18:27:54 +0700

    asas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 const Home=()=>{
     return(
         <div style={{backgroundColor:'gray',flex:1,justifyContent:'center',alignItems:'center',width:'100%',height:500}}>
-            INI HOME
+            
         </div>
     )
 }
