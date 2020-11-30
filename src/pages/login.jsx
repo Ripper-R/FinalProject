b2a8659e6d921197b3d42799b66d4dca49e9f868
Ripper-R <68725194+Ripper-R@users.noreply.github.com>
2020-11-30 20:36:05 +0700

    login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
-
+import FormControl from '@material-ui/core/FormControl'
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import PersonIcon from '@material-ui/icons/Person';
+import LockIcon from '@material-ui/icons/Lock';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import IconButton from '@material-ui/core/IconButton';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 const LoginScreen=()=>{
+    const [values, setValues] = React.useState({
+        password:'',
+        username:''
+    })
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+    const handleClickShowPassword = () => {
+        setValues({ ...values, showPassword: !values.showPassword });
+    };
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+    
+    
     return(
-        <div>
-            INI LOGIN
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+            <div style={{marginTop:100,flexDirection:'column',display:'flex'}}>
+                <FormControl>
+                    <InputLabel htmlFor="input-with-icon-adornment">Username</InputLabel>
+                    <Input
+                    id="input-with-icon-adornment"
+                    value={values.username}
+                    startAdornment={
+                    <InputAdornment position="start">
+                        <PersonIcon/>
+                    </InputAdornment>
+                     }
+                    />
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor="input-with-icon-adornment">Password</InputLabel>
+                    <Input
+                            id="standard-adornment-password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            startAdornment={
+                                <InputAdornment position='start'>
+                                    <LockIcon/>
+                                </InputAdornment>
+                            }
+                            endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                >
+                                {values.showPassword ? <VisibilityIcon /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                            }
+                        />
+                </FormControl>
+
+            </div>
             
             
         </div>
