6d0653de0252547a618ffe8d312a1f1bb74c50a2
Ripper-R <68725194+Ripper-R@users.noreply.github.com>
2020-11-30 12:01:17 +0700

    Header improvement

--- src/pages/components/Headers.jsx
+++ src/pages/components/Headers.jsx
@@ -14,25 +14,33 @@ import {
   NavbarText
 } from 'reactstrap';
 import './header.css'
-
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from '@material-ui/core/styles';
 const Example = (props) => {
+  const useStyles = makeStyles((theme) => ({
+    margin: {
+      marginLeft:0,
+      marginRight: 20
+    },
+  }));
   const [isOpen, setIsOpen] = useState(false);
-
   const toggle = () => setIsOpen(!isOpen);
-
+  const classes = useStyles();
   return (
     <div class='Head'>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarBrand href="/">Testing</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Components</NavLink>
+              <NavLink href='/menu'>Menu</NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
+            
             <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav caret>
                 Options
@@ -51,7 +59,23 @@ const Example = (props) => {
               </DropdownMenu>
             </UncontrolledDropdown>
           </Nav>
-          <NavbarText>HEADER</NavbarText>
+          <div>
+            ARAta
+          </div>
+          <FormControl className={classes.margin}>
+        <InputLabel htmlFor="input-with-icon-adornment">Search</InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon/>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+          <div style={{marginLeft:10}}>Login</div>
+          <div style={{marginLeft:10}}>Register</div>
+          <NavbarText style={{marginLeft:10}}>HEADER</NavbarText>
         </Collapse>
       </Navbar>
     </div>
