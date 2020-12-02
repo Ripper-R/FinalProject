ac8d193f2da13a42e481e9ebc1ea26225266a4a9
Ripper-R <68725194+Ripper-R@users.noreply.github.com>
2020-12-02 20:22:06 +0700

    PRODUCT LIST CAROUSEL plus footer

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,33 +5,22 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Carousel from 'react-material-ui-carousel'
-import {Paper} from '@material-ui/core'
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Footer from './../components/Footer'
   const product=()=>{
-            function Example(props)
-        {
-            var items = [
-                {
-                    name: "Random Name #1",
-                    description: "Probably the most random thing you have ever seen!"
-                },
-                {
-                    name: "Random Name #2",
-                    description: "Hello World!"
-                }
-            ]
-        
-            return (
-                <Carousel>
-                    {
-                        items.map( (item, i) => <Item key={i} item={item} /> )
-                    }
-                </Carousel>
-            )
-        }
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
     const getproduct=(product,index)=>{
         return(
+
+
             <Card style={{flexBasis:'18%',marginTop:10,marginRight:5}} >
             <CardContent>
               <Typography  color="textSecondary" gutterBottom>
@@ -56,7 +45,18 @@ import {Paper} from '@material-ui/core'
     }
     
     return(
-        <div style={{width:'100%',height:'100%',borderWidth:'3px',borderStyle:'solid',backgroundColor:'gray'}}>
+      <div style={{width:'100%',height:'100%',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+           <div style={{width:600,height:300,marginBottom:20,marginTop:20}}>
+              <Slider {...settings}>
+                  <div >
+                     <img style={{width:600,height:300}} src='./images/img-1.jpg'/>
+                  </div>
+                  <div>
+                      <h3>2</h3>
+                  </div>
+                    </Slider>
+            </div>
+        <div style={{width:'100%',height:'100%',borderWidth:'3px',borderStyle:'solid',backgroundColor:'gray',marginTop:20,marginBottom:20}}>
             <div style={{flexWrap:'wrap',display:'flex',padding:20,justifyContent:'space-evenly'}}>
               {
                   products.map((product, index) => {
@@ -65,6 +65,11 @@ import {Paper} from '@material-ui/core'
               }
             </div>
         </div>
+        <div style={{width:'100%'}}>
+
+        <Footer/>
+        </div>
+      </div>
     )
 }
 
